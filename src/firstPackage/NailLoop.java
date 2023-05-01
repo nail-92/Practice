1bd02f015af38db4f0fe91ff70811009b93dafaf
roman <romanaliev808@gmail.com>
2023-04-30 21:31:38 -0500

    new Loop updation

---
+++ src/firstPackage/NailLoop.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+public class NailLoop {
+    public static void main(String[] args) {
+
+        int u = 1;
+
+        while (u < 0){
+            System.out.println("Hello Nail");
+        }
+        System.out.println("It`s updated");
+    }
+}
