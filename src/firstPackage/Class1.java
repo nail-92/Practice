18f7495c275110017198ef7d6b5557bfa3b43fe6
nail-92 <nail-92@live.com>
2023-04-30 20:43:36 -0500

    roma smotri

--- src/firstPackage/Class1.java
+++ src/firstPackage/Class1.java
@@ -3,5 +3,8 @@ package firstPackage;
 public class Class1 {
     public static void main(String[] args) {
 
+        for (int i = 0; ; i++) {
+            System.out.println("Roma kuku ;)");
+        }
     }
 }
