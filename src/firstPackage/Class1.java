ee7bd8b1a4b969587e594ffa53d4819e6f301710
roman <romanaliev808@gmail.com>
2023-04-30 20:42:13 -0500

    updated

--- src/firstPackage/Class1.java
+++ src/firstPackage/Class1.java
@@ -1,7 +1,7 @@
-import utils;
+package firstPackage;
 
 public class Class1 {
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
 
         String hi = "Hello Nail";
 
